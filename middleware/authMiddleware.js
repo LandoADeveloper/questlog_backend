b8e0043688788cfe16e98965a6aa2d294801040d
Lando Meyer <121017979+LandoADeveloper@users.noreply.github.com>
2023-04-25 14:04:57 +0200

    Update authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import {createHmac} from 'crypto'
+import { verifyJWTToken } from '../util/token.js'
+
+export const encryptPassword = (req, _, next) => {
+    const hmac = createHmac('sha256', req.body.password)
+    req.body.password = hmac.digest('hex')
+    next()
+}
+
+export const verifyJWTCookie = (req, res, next) => {
+    const token = req.cookie.token
+    try{
+        const claim = verifyJWTToken(token)
+        req.claim = claim
+        console.log('claim: ' + req.claim);
+        next()
+    } catch(e){
+        console.log(e.message);
+        res.status(404).end()
+    }
+}
